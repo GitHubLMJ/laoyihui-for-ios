e83a66eeb32bf87d9ef4e2088774eddf2c828494
GitHubLMJ <1300074136@qq.com>
2015-10-06 09:48:23 +0800

    othertest

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by X on 15/10/6.
+//  Copyright © 2015年 LMJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by X on 15/10/6.
+//  Copyright © 2015年 LMJ. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
